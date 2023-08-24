73367088be5c30ceaef2cc4d001996f2ffacfd47
tascheidt <88148089+tascheidt@users.noreply.github.com>
2023-08-24 18:07:45 -0400

    Update apikeys.py
    Placeholders only

--- apikeys.py
+++ apikeys.py
@@ -2,8 +2,9 @@
 # OpenAI API Key; we use GPT-4 in this demo
 # Google API Key for using its web search components + a search ID
 # Our search ID has a global web context; i.e., we simply use the base Google search offering
+# The maps_api_key is available from the Google Cloud Developers console
 
-openai_key = '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'
-google_api_key = 'AIzaSyDAJEjbQRwZnC1L3O7PoddpJEedr-jG0_o'
-search_id = 'f5da9b45ac43349df'
-maps_api_key = 'AIzaSyCB5ATNC6NVz0AZvsBybZg_SNiMfZEtSlQ'
+openai_key = 'your api key'
+google_api_key = 'your api key'
+search_id = 'your api key'
+maps_api_key = 'your api key'
